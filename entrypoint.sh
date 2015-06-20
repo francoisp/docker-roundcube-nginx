ae68b58e393c50b7c704dbdbe5e24da3b3cca5d1
francoisp <francoisp@netmosphere.net>
2015-06-20 09:10:26 -0400

    entrypoint updates postgres db ip in roundcube config to allow nginx restarts post pg restart and internal ip change

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,9 +52,105 @@ fi
 echo ">> copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
 cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null
 
+
+#RC CONFIG
+if [ -z ${POSTGRES_USER+x} ] || [ -z ${PGPASSWORD+x} ]
+then
+  >&2 echo ">> no user or password for database specified!"
+  exit 1
+fi
+
+if [ -z ${ROUNDCUBE_IMAP_HOST+x} ]
+then
+  ROUNDCUBE_IMAP_HOST=mail
+fi
+
+if [ -z ${ROUNDCUBE_IMAP_PROTO+x} ]
+then
+  ROUNDCUBE_IMAP_PROTO=tls
+fi
+
+if [ -z ${ROUNDCUBE_SMTP_HOST+x} ]
+then
+  ROUNDCUBE_SMTP_HOST=mail
+fi
+
+if [ -z ${ROUNDCUBE_SMTP_PROTO+x} ]
+then
+  ROUNDCUBE_SMTP_PROTO=tls
+fi
+
+if [ -z ${ROUNDCUBE_SMTP_PORT+x} ]
+then
+  ROUNDCUBE_SMTP_PORT=25
+fi
+
+if [ -z ${ROUNDCUBE_LANGUAGE+x} ]
+then
+  ROUNDCUBE_LANGUAGE=en_CA
+fi
+
+
+if [ -z ${POSTGRES_PORT_5432_TCP_PORT+x} ]
+then
+  POSTGRES_PORT_5432_TCP_PORT=5432
+fi
+
+if [ -z ${PG_DBNAME+x} ]
+then
+  PG_DBNAME=roundcube
+fi
+
+if [ -z ${ROUNDCUBE_PHP_DATE_TIMEZONE+x} ]
+then
+  ROUNDCUBE_PHP_DATE_TIMEZONE=America/Montreal
+fi
+
+if [ -z ${ROUNDCUBE_RELATIVE_URL_ROOT+x} ]
+then
+  ROUNDCUBE_RELATIVE_URL_ROOT="/"
+fi
+
+#Roundcube Webmail
+if [ -z ${ROUNDCUBE_NAME+x} ]
+then
+  ROUNDCUBE_NAME=Roundcube Webmail
+fi
+
+
+ROUNDCUBE_RANDOM=`perl -e 'my @chars = ("A".."Z", "a".."z"); my $string; $string .= $chars[rand @chars] for 1..24; print $string;'` # returns exactly 24 random chars
+
+###
+# Configuration
+###
+if [ ! -e "/roundcube/config/TPLconfig.inc.php" ] 
+then
+  cp /roundcube/config/config.inc.php /roundcube/config/TPLconfig.inc.php
+fi
+cp /roundcube/config/TPLconfig.inc.php /roundcube/config/config.inc.php
+
+sed -i "s/PG_USER/$POSTGRES_USER/g" /roundcube/config/config.inc.php
+sed -i "s/PG_PASSWORD/$PGPASSWORD/g" /roundcube/config/config.inc.php
+sed -i "s/PG_DB/$PG_DBNAME/g" /roundcube/config/config.inc.php
+sed -i "s/PG_TCP_ADDR/$POSTGRES_PORT_5432_TCP_ADDR/g" /roundcube/config/config.inc.php
+sed -i "s/PG_PORT/$POSTGRES_PORT_5432_TCP_PORT/g" /roundcube/config/config.inc.php
+sed -i "s/IMAP_HOST/$ROUNDCUBE_IMAP_HOST/g" /roundcube/config/config.inc.php
+sed -i "s/IMAP_PROTOCOL/$ROUNDCUBE_IMAP_PROTO/g" /roundcube/config/config.inc.php
+sed -i "s/SMTP_HOST/$ROUNDCUBE_SMTP_HOST/g" /roundcube/config/config.inc.php
+sed -i "s/SMTP_PROTOCOL/$ROUNDCUBE_SMTP_PROTO/g" /roundcube/config/config.inc.php
+sed -i "s/SMTP_PORT/$ROUNDCUBE_SMTP_PORT/g" /roundcube/config/config.inc.php
+sed -i "s/LOCALISATION/$ROUNDCUBE_LANGUAGE/g" /roundcube/config/config.inc.php
+sed -i "s/ROUNDCUBE_RANDOM/$ROUNDCUBE_RANDOM/g" /roundcube/config/config.inc.php
+sed -i "s/Roundcube Webmail/$ROUNDCUBE_NAME/g" /roundcube/config/config.inc.php
+
+echo ">> set Timezone -> $ROUNDCUBE_PHP_DATE_TIMEZONE"
+sed -i "s!;date.timezone =.*!date.timezone = $ROUNDCUBE_PHP_DATE_TIMEZONE!g" /etc/php5/fpm/php.ini
+
 # exec CMD
 
 /opt/startup-roundcube.sh
+
+
 echo ">> exec docker CMD"
 echo "$@"
 exec "$@"
